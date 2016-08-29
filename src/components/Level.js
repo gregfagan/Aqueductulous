044c07bcdfae2ac003b8f66245f7cd439bd50305
Alan Wong <flyingwok@gmail.com>
2016-08-29 00:08:42 -0700

    High fidelity aquaduct track with hazard zones.

--- src/components/Level.js
+++ src/components/Level.js
@@ -25,55 +25,108 @@ export default function Level ({curve, hazards, xOffset, unitLength}) {
   // Move cursor to curveLeftIndex's left origin point, which is
   // inferred from the endpoint of curve[curveLeftIndex - 1]. So in the
   // curveLeftIndex == 0 case, the origin is 0, 4.5, the player start point.
+  // DrawingOriginPoint fakes a bezier curve description to simplify the loop
   let drawingOriginPoint = {
-    x: curveLeftIndex === 0 ? 0 : curve[curveLeftIndex - 1].endpoint.x,
-    y: curveLeftIndex === 0 ? 4.5 : curve[curveLeftIndex - 1].endpoint.y
+    controlPoint1: {x: NaN, y: NaN},  // not used
+    controlPoint2: {x: NaN, y: NaN},  // not used
+    endpoint: {
+      x: curveLeftIndex === 0 ? 0 : curve[curveLeftIndex - 1].endpoint.x,
+      y: curveLeftIndex === 0 ? 4.5 : curve[curveLeftIndex - 1].endpoint.y
+    }
   }
 
-  // Cuz typing this repeatedly sucks.
-  function screenifyX(x) { return (x - trackGameWindowLeft) * unitLength; }
-  function screenifyY(y) { return y * unitLength; }
-
-  let visibleCurve = curve.slice(curveLeftIndex, curveRightIndex + 1)
-  let visibleHazards = hazards.slice(curveLeftIndex, curveRightIndex + 1);
-
-  // Draw each curve segment separately...
-  // Draw origin, draw first curve
-  // Each subsequent curve uses the previous curve endpoint as the origin
-
-  let hazardColor = 0xF00;
-  let regularColor = 0xFFF;
+  let visibleCurve = [drawingOriginPoint];
+  Array.prototype.push.apply(visibleCurve, curve.slice(curveLeftIndex, curveRightIndex + 1));
+  
+  let visibleHazards = [false];
+  Array.prototype.push.apply(visibleHazards, hazards.slice(curveLeftIndex, curveRightIndex + 1)); // first element faked to match the fake drawing origin point
 
   return (
     <Group>
-      <Shape
-        d={`
-          M ${ screenifyX(drawingOriginPoint.x) } ${ screenifyY(drawingOriginPoint.y) }
-          C ${ screenifyX(visibleCurve[0].controlPoint1.x) } ${ screenifyY(visibleCurve[0].controlPoint1.y) }
-            ${ screenifyX(visibleCurve[0].controlPoint2.x) } ${ screenifyY(visibleCurve[0].controlPoint2.y) }
-            ${ screenifyX(visibleCurve[0].endpoint.x) } ${ screenifyY(visibleCurve[0].endpoint.y) }
-        `}
-        stroke={ visibleHazards[0] ? hazardColor : regularColor }
-        strokeWidth={10}
-      />
-      { visibleCurve.map((value, index, array) => {
-          if (index !== 0) {
-          return (
-            <Shape
-              d={`
-                M ${ screenifyX(array[index - 1].endpoint.x) }
-                  ${ screenifyY(array[index - 1].endpoint.y) }
-                C ${ screenifyX(value.controlPoint1.x) } ${ screenifyY(value.controlPoint1.y) }
-                  ${ screenifyX(value.controlPoint2.x) } ${ screenifyY(value.controlPoint2.y) }
-                  ${ screenifyX(value.endpoint.x) } ${ screenifyY(value.endpoint.y) }
-                `}
-              stroke={ visibleHazards[index] ? hazardColor : regularColor }
-              strokeWidth={10}
+      { visibleCurve.map((value,index,array) => {
+        return (index !== 0)
+          ? (
+            <AquaductSegment
+              key={index}
+              originPoint={ {x: array[index - 1].endpoint.x, y: array[index - 1].endpoint.y} }
+              bezierCurve={ array[index] }
+              isHazard={ visibleHazards[index] }
+              windowLeft={ trackGameWindowLeft }
+              unitLength={ unitLength }
             />
-            );
-          }
-        })
-      }
+          )
+          : undefined;
+        }
+      )}
     </Group>
+  )
+}
+
+function AquaductSegment({originPoint, bezierCurve, isHazard, windowLeft, unitLength}) {
+  return (
+    <Group>
+      <WideAquaductSegment
+        originPoint={originPoint}
+        bezierCurve={bezierCurve}
+        isHazard={isHazard}
+        windowLeft={windowLeft}
+        unitLength={unitLength}
+      />
+      <NarrowAquaductSegment
+        originPoint={originPoint}
+        bezierCurve={bezierCurve}
+        isHazard={isHazard}
+        windowLeft={windowLeft}
+        unitLength={unitLength}
+      />
+    </Group>
+  );
+}
+
+function WideAquaductSegment({originPoint, bezierCurve, isHazard, windowLeft, unitLength}) {
+  return (
+    <GenericAquaduct
+      originPoint={originPoint}
+      bezierCurve={bezierCurve}
+      isHazard={isHazard}
+      windowLeft={windowLeft}
+      unitLength={unitLength}
+      strokeWidth={60}
+      strokeColor={0xAAA}
+    />
+  );
+}
+
+function NarrowAquaductSegment({originPoint, bezierCurve, isHazard, windowLeft, unitLength}) {
+  const strokeColor = isHazard ? 0xF00 : 0x999;
+  return (
+    <GenericAquaduct
+      originPoint={originPoint}
+      bezierCurve={bezierCurve}
+      isHazard={isHazard}
+      windowLeft={windowLeft}
+      unitLength={unitLength}
+      strokeWidth={30}
+      strokeColor={strokeColor}
+    />  
+  );
+}
+
+function GenericAquaduct({originPoint, bezierCurve, isHazard, windowLeft, unitLength, strokeWidth, strokeColor}) {
+  // Cuz typing this repeatedly sucks.
+  function screenifyX(x) { return (x - windowLeft) * unitLength; }
+  function screenifyY(y) { return y * unitLength; }
+
+  return (
+    <Shape
+      d={`
+        M ${ screenifyX(originPoint.x) } ${ screenifyY(originPoint.y) }
+        C ${ screenifyX(bezierCurve.controlPoint1.x) } ${ screenifyY(bezierCurve.controlPoint1.y) }
+          ${ screenifyX(bezierCurve.controlPoint2.x) } ${ screenifyY(bezierCurve.controlPoint2.y) }
+          ${ screenifyX(bezierCurve.endpoint.x) } ${ screenifyY(bezierCurve.endpoint.y) }
+      `}
+      stroke={ strokeColor }
+      strokeWidth={ strokeWidth }
+    />
   );
 }
